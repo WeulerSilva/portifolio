d1f10ee0b0fdca295f2243faa64f16cc01121c40
WeulerSilva <weulergamer@outlook.com>
2023-06-16 06:11:52 -0300

    mudei o caminho de audio

--- assets/Js/drum.js
+++ assets/Js/drum.js
@@ -76,7 +76,7 @@ document.body.addEventListener('keyup', (event) => {
 });
 
 function playSound(sound) {
-    let audioElement = new Audio(`assets/Media/sound/${sound}.wav`)
+    let audioElement = new Audio(`https://github.com/WeulerSilva/portifolio/blob/main/assets/Media/sound/${sound}.wav`)
 
     if(audioElement) {
         audioElement.play() 
