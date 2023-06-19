a516354e95e332efffbdea3235afeb1f7b9969b0
WeulerSilva <weulergamer@outlook.com>
2023-06-19 06:56:51 -0300

    consertando o botão de voltar
    Agora e possivel voltar para o menu apertando no botão de voltar no menu,atraves de um sistema de currentBack.

--- assets/Js/phone.js
+++ assets/Js/phone.js
@@ -11,6 +11,7 @@ const career = document.querySelector('.career');
 const github = document.querySelector('.github');
 const linkedin = document.querySelector('.linkedin');
 const calculator = document.querySelector('.calculator');
+let currentBack = 0;
 
 //funções responsaveis pela hora
 
@@ -35,20 +36,30 @@ updateClock();
 
 setInterval(updateClock,60000);
 
+function buttonBack () {
+    if(currentBack <= 0) {
+        firstScreen.classList.replace('none', 'flex');
+        secondScreen.classList.replace('flex', 'none');
+        bottons.classList.replace('flex', 'none');
+    }
+}
+
 
 function backMenu (item) {
     back.addEventListener('click', () => {
         secondScreen.classList.replace('none', 'flex');
         item.classList.replace('flex', 'none');
+        buttonBack();
     })
 }
 
 function backHomi (item) {
     homi.addEventListener('click', () => {
         firstScreen.classList.replace('none', 'flex');
-        secondScreen.classList.replace('flex', 'none')
+        secondScreen.classList.replace('flex', 'none');
         item.classList.replace('flex', 'none');
     })
+
 }
 
 //botão ir para o menu
@@ -56,6 +67,8 @@ menu.addEventListener('click',() => {
     firstScreen.classList.replace('flex', 'none');
     secondScreen.classList.replace('none', 'flex');
     bottons.classList.replace('none', 'flex');
+    currentBack ++;
+    buttonBack();
 });
 
 //botão voltar para o homi
@@ -64,12 +77,16 @@ homi.addEventListener('click', () => {
     firstScreen.classList.replace('none', 'flex');
     bottons.classList.replace('flex','none');
     camera.classList.remove('camera-img');
+    currentBack = 0;
+    buttonBack();
 });
 
 //botão de volta
 back.addEventListener('click', () => {
     secondScreen.classList.replace('none', 'flex');
     camera.classList.remove('camera-img');
+    currentBack --;
+    buttonBack();
 })
 
 //app de clima
@@ -78,6 +95,8 @@ document.querySelector('.i-weather').addEventListener('click', () => {
     weather.classList.replace('none', 'flex');
     backMenu(weather);
     backHomi(weather);
+    currentBack ++;
+    buttonBack();
 })
 
 //app de camera
@@ -87,6 +106,7 @@ document.querySelector('.i-camera').addEventListener('click', () => {
     weather.classList.replace('flex', 'none');
     backMenu(camera);
     backHomi(camera);
+    currentBack++;
 })
 
 document.querySelector('.camera-front').addEventListener('click',() => {
@@ -99,6 +119,7 @@ document.querySelector('.i-carrer').addEventListener('click', () => {
     career.classList.replace('none', 'flex');
     backMenu(career);
     backHomi(career);
+    currentBack++;
 });
 
 //app github
@@ -107,6 +128,7 @@ document.querySelector('.i-github').addEventListener('click', () => {
     github.classList.replace('none', 'flex');
     backMenu(github);
     backHomi(github);
+    currentBack++;
 })
 
 //app lindekin
@@ -115,6 +137,7 @@ document.querySelector('.i-linkedin').addEventListener('click', () => {
     linkedin.classList.replace('none', 'flex');
     backMenu(linkedin);
     backHomi(linkedin);
+    currentBack++;
 })
 
 //app calculator
@@ -123,4 +146,6 @@ document.querySelector('.i-calculator').addEventListener('click', () => {
     calculator.classList.replace('none', 'flex');
     backMenu(calculator);
     backHomi(calculator);
+    currentBack++;
 })
+
