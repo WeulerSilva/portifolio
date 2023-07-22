630fee7fff20fbfec519354f11ff18b004e2c88e
WeulerSilva <weulergamer@outlook.com>
2023-07-22 20:37:42 -0300

    calendario totalmente feito

--- assets/Js/calendar.js
+++ assets/Js/calendar.js
@@ -5,10 +5,59 @@ let data = new Date();
 let dayNumber = data.getDay();
 let yearNumber = data.getFullYear();
 let monthYear = month[data.getMonth()];
-console.log(monthYear)
+let numberMonth = data.getDate();
+
 
 let active = document.querySelector(".week li:nth-child("+dayNumber+")");
 active.classList.add('current');
 
 let dYear = document.querySelector('.calendar span');
-dYear.innerHTML = yearNumber;
+let dDay = document.querySelector('.calendar-day');
+let dMonth = document.querySelector('.calendar h3');
+
+function currentData() {
+    dYear.innerHTML = yearNumber;
+    dDay.innerHTML = numberMonth;
+    dMonth.innerHTML = monthYear;
+}
+
+function atualizarDiaAtual() {
+    // Primeiro, remove a classe 'current' de qualquer elemento que a possua
+    const currentElements = document.querySelectorAll('.current');
+    currentElements.forEach((element) => {
+      element.classList.remove('current');
+    });
+  
+    let dayName = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'];
+    let todayName = dayName[data.getDay()];
+
+    let index = dayName.indexOf(todayName);
+
+    if(index === 0) {
+        index = 7;
+    }
+
+    const active = document.querySelector(`.week li:nth-child(${index})`);
+    active.classList.add('current');
+  }
+
+function changeData(days) {
+    data.setDate(data.getDate() + days);
+    dayNumber = data.getDay();
+    yearNumber = data.getFullYear();
+    monthYear = month[data.getMonth()];
+    numberMonth = data.getDate();
+
+    atualizarDiaAtual(); 
+    currentData();
+}
+
+currentData();
+
+document.querySelector('.calendar-next').addEventListener('click', () => {
+    changeData(1);
+})
+
+document.querySelector('.calendar-prev').addEventListener('click', () => {
+    changeData(-1);
+})
