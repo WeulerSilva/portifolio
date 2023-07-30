f132a55c5183b3fc9a51f0ae531271526eaf666b
WeulerSilva <weulergamer@outlook.com>
2023-07-30 13:42:02 -0300

    adicionando a função de mudar o idioma pelas configurações

--- assets/Js/config.js
+++ assets/Js/config.js
@@ -1,1 +1,25 @@
-console.log('funciona')
+if(languageEng) {
+  document.querySelector('.config h1').innerHTML = 'Settings';
+  document.querySelector('.config p').innerHTML = 'Change language:';
+  document.querySelector('.config button').innerHTML = 'Make change';
+}
+
+
+document.querySelector('.config-lang').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let selectOp = document.querySelector('.config-language').value;
+
+  localStorage.setItem('languageEng', selectOp);
+
+  document.querySelector('.config-lang button').addEventListener('click', changeLang);
+});
+
+function changeLang() {
+  let languageEng = localStorage.getItem('languageEng') === 'true';
+  location.reload();
+
+  console.log(languageEng)
+}
+
+
